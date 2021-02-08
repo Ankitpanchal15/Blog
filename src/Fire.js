bd31a4fc9376252424c50839b7c0abaefe934ef0
Ankit Panchal <76251693+Ankitpanchal15@users.noreply.github.com>
2021-02-08 22:08:47 +0530

    Update Fire.js

--- src/Fire.js
+++ src/Fire.js
@@ -19,44 +19,10 @@ import 'firebase/storage'
   const auth = firebase.auth();
   const storage = firebase.storage();
   const timestamp = firebase.firestore.FieldValue.serverTimestamp;
-
-  var provider = new firebase.auth.GoogleAuthProvider();
-
-  const signInWithPopup = () => {
-    firebase.auth()
-  .signInWithRedirect(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
-
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-  };
-
-  const signOut = firebase.auth().signOut().then(() => {
-                    // Sign-out successful.
-                  }).catch((error) => {
-                    // An error happened.
-                  });
+ 
 
 
-  
-
-
-  export  {db,auth,storage, timestamp, signInWithPopup}
+  export  {db,auth,storage, timestamp}
 
 
 
